1e16cff3c139f4a57f436b6f7620bc196d93d67a
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-09 23:13:15 +0900

    LH7n: AxionAOSP Bringup
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_LH7n.mk
+++ lineage_LH7n.mk
@@ -26,5 +26,36 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=LH7n \
     BuildFingerprint=TECNO/LH7n-GL/TECNO-LH7n:14/UP1A.231005.007/250416V781:user/release-keys
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+# AxionAOSP Flags
+AXION_CAMERA_REAR_INFO := 50
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := Shirayuki39
+AXION_PROCESSOR := Helio_G99
+PRODUCT_NO_CAMERA := false
+BYPASS_CHARGE_SUPPORTED := false
+
+# Define small and big core groups
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+
+## CPUsets configuration
+# CPUset used for bg/audio cpusets 
+AXION_CPU_BG := 0-2
+
+# CPUset used for foreground cpusets
+AXION_CPU_FG := 2-7
+
+# CPUset that will be used when limiting other cpusets except top-app
+AXION_CPU_LIMIT_BG := 0-1
+
+# CPUset that will be used to unlimit critical cpusets for UI
+AXION_CPU_UNLIMIT_UI := 0-7
+
+# CPUset that will be used when limiting critical cpusets for UI
+AXION_CPU_LIMIT_UI := 2-5
+
+# CPUset that will be used for critical display processes
+AXION_CPU_DISPLAY := 6-7
+
+# CPUset that will be used for audio processes e.g. audioserver
+AXION_CPU_AUDIO := 0-3
